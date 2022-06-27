e70bd44fdd229bec6ea65ac2a4c758a5a30455b3
ItsJuanito <jzaragoza9jr@gmail.com>
2022-06-27 12:14:24 -0700

    added comments

--- Queue/PriorityQueue.py
+++ Queue/PriorityQueue.py
@@ -1,23 +1,32 @@
+'''
+Priority Queue is an abstract data type that is similar to a queue, and every element has 
+some priority value associated with it. The priority of the elements in a priority queue 
+determines the order in which elements are served. If in any case the elements have same 
+priority, they are served as per their ordering in the queue.
+'''
+
+# Priority Queue class that contains a queue list and methods 
 class PQueue:
     queue = []
+    # this method checks to see if the priority queue is empty or not
     def isEmpty(self):
         if len(self.queue) == 0:
             return True
         else:
             return False
-    
+    # this method adds an item to the priority queue
     def push(self, priority, data):
         if priority < 0:
             return print("Invalid priority entry!")
         temp = (priority, data)
         self.queue.append(temp)
-
+    # this method removes and returns the prioritized value
     def pop(self):
         self.queue.sort(key=lambda q: q[0])
         data = self.queue[0][1]
         del self.queue[0]
         return data
-
+    # this method prints the priority queue
     def print(self):
         self.queue.sort(key=lambda q: q[0])
         for tuple in self.queue:
@@ -25,16 +34,19 @@ class PQueue:
         print()
 
 if __name__ == "__main__":
+    # instance of PQueue class
     pq = PQueue()
+    # isEmpty, returns true if priority queue is empty, and false if it is not empty
     print(pq.isEmpty())
+    # push, adds an item with a priority to the priority queue 
     pq.push(-1, 19)
     pq.push(10, 4)
     pq.push(1, 18)
     pq.push(3, 1)
     pq.push(1, 12)
-
+    # print, simlpy prints the priority queue
     pq.print()
-
+    # pop, returns and deleted the prioritized value
     popped = pq.pop()
     print(popped)
     print(pq.isEmpty())
--- Queue/Queue.py
+++ Queue/Queue.py
@@ -1,19 +1,30 @@
+'''
+A Queue is a linear structure which follows a particular order in which the operations 
+are performed. The order is First In First Out (FIFO). A good example of a queue is any 
+queue of consumers for a resource where the consumer that came first is served first. 
+The difference between stacks and queues is in removing. In a stack we remove the item 
+the most recently added; in a queue, we remove the item the least recently added.
+'''
+
+# Queue class contains a queue list and methods
 class Queue:
     queue = []
+    # this method returns the string values of the queue
     def __repr__(self) -> str:
         return str(self.queue)
+    # this method adds data to the queue
     def enqueue(self, data):
         self.queue.insert(0, data)
-
+    # this method returns and deletes the last value in the queue
     def dequeue(self):
         return self.queue.pop()
-    
+    # this method returns the size of the queue
     def size(self):
         return len(self.queue)
-    
+    # this method returns the last element(tail) in the queue
     def peek(self):
         return self.queue[len(self.queue) - 1]
-    
+    # this method returns true if the queue is empty and false if it is not
     def isEmpty(self):
         if len(self.queue) == 0:
             return True
@@ -21,10 +32,11 @@ class Queue:
             return False
 
 if __name__ == "__main__":
+    # instance of Queue class
     q = Queue()
-    
+    # isEmpty, returns true of queue is empty, false if it is not
     print(q.isEmpty())
-    
+    # enqueue, adds an element to the queue
     q.enqueue(10)
     q.enqueue(3)
     q.enqueue(24)
@@ -32,15 +44,14 @@ if __name__ == "__main__":
     q.enqueue(11)
 
     print(q)
-    
+    # dequeue, removes the last element in the queue
     print(q.dequeue())
-    
+    # returns the last element of the queue
     print(q.peek())
     
     print(q.isEmpty())
     
     q.enqueue(18)
-    
     print(q)
 
 '''
