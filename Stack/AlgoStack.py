f70b0cc066c3b9bdb3c7c2f78812c91428f11f9f
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-05 13:45:36 -0700

    added funciton

--- Stack/AlgoStack.py
+++ Stack/AlgoStack.py
@@ -15,19 +15,34 @@ def validParenthesis(string):
                 ((s == "]") and (lastParentheses != "["))):
                 return False
     return True
+# This function returns a reversed version of the string
+def reverseString(string):
+    if len(string) == 0:
+        return string
+    stack = Stack()
+    for i in string:
+        stack.push(i)
+    result = ""
+    while stack.size() != 0:
+        result += stack.pop()
+    return result
 
 # Test Stack methods
 if __name__ == "__main__":
     string = "({[]})"
     string2 = "(}"
     string3 = "[]()"
+    string4 = "restart"
     print(validParenthesis(string))
     print(validParenthesis(string2))
     print(validParenthesis(string3))
+    
+    print(reverseString(string4))
 
 '''
 Sample Output:
  - True
  - False
  - True
+ - tratser
 '''
