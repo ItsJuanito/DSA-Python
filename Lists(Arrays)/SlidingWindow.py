49e329e4165f9169dadc8853507741d86b3dd61f
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-06 13:34:37 -0700

    added list folder and module

---
+++ Lists(Arrays)/SlidingWindow.py
@@ -0,0 +1,101 @@
+'''
+1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+
+(Medium)
+
+Problem Statement:
+Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
+
+Example 1:
+Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+Output: 3
+Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively. All other sub-arrays of size 3 have averages less than 4 (the threshold).
+
+Example 2:
+Input: arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+Output: 6
+Explanation: The first 6 sub-arrays of size 3 have averages greater than 5. Note that averages are not integers.
+'''
+
+def numOfSubarrays(arr, k, threshold):
+    # initiate variables
+    sum = 0
+    update = 0
+    count = 0
+    # loop through the arr
+    for window in range(len(arr)):
+        # update the sum
+        sum += arr[window]
+        # check to see if iterator is greater than k - 1
+        if window >= k - 1:
+            # get calculate the average
+            average = sum // k
+            # subtract from sum (sliding window)
+            sum -= arr[update] 
+            # increment update
+            update += 1
+            # check if average is >= threshold
+            if average >= threshold:
+                # increment count
+                count += 1
+    # return count
+    return count
+    
+arr = [2,2,2,2,5,5,5,8]
+k = 3
+threshold = 4
+print(str(numOfSubarrays(arr, k, threshold)))
+# Output: 3
+arr2 = [11,13,17,23,29,31,7,5,2,3]
+k2 = 3
+threshold2 = 5
+print(str(numOfSubarrays(arr2, k2, threshold2)))
+# Output : 6
+
+'''
+Number of Sub-arrays of Size K with Greatest Sum.
+
+(Easy)
+
+Problem Statement:
+Given an array of positive numbers and a positive number k, find the maximum sum of any contiguous subarray of size k.
+
+Example 1:
+Input: [2, 1, 5, 1, 3, 2], k=3 
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+
+Example 2:
+Input: [2, 3, 4, 1, 5], k=2 
+Output: 7
+Explanation: Subarray with maximum sum is [3, 4].
+'''
+
+def max_sub_array_of_size_k(arr, k):
+    # initiate variables
+    window_sum = 0
+    max_sum = 0
+    update = 0
+    # loop through the array
+    for window in range(len(arr)):
+        # add item to sum
+        window_sum += arr[window]
+        # check if iterator is >= k - 1
+        if window >= k - 1:
+            # update max_sum and compare sums
+            max_sum = max(max_sum, window_sum)
+            # subtract from sum (sliding window)
+            window_sum -= arr[update]
+            # increment update
+            update += 1
+    # return max_sum
+    return max_sum
+
+arr = [2, 1, 5, 1, 3, 2]
+k = 3 
+print(str(max_sub_array_of_size_k(arr, k)))
+# Output: 9
+arr2 = [2, 3, 4, 1, 5]
+k2 = 2
+print(str(max_sub_array_of_size_k(arr2, k2)))
+# Output: 7
