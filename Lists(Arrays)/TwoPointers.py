d210559f3375c40f00a1eb61f82b587dd84c7c35
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-11 12:55:36 -0700

    added twosum funciton

--- Lists(Arrays)/TwoPointers.py
+++ Lists(Arrays)/TwoPointers.py
@@ -4,9 +4,10 @@
 (Medium)
 
 Problem Statement:
-You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-Find two lines that together with the x-axis form a container, such that the container contains the most water.
-Return the maximum amount of water a container can store.
+You are given an integer array height of length n. There are n vertical lines drawn such that the two 
+endpoints of the ith line are (i, 0) and (i, height[i]). Find two lines that together with the x-axis 
+form a container, such that the container contains the most water. Return the maximum amount of water a 
+container can store.
 
 Example 1:
 Input: height = [1,8,6,2,5,4,8,3,7]
@@ -48,3 +49,54 @@ print(maxArea(height))
 height2 = [1, 1]
 print(maxArea(height2))
 # Output: 1
+
+'''
+Sorted Two Sum
+
+(Easy)
+
+Problem Statement:
+Given a sorted array of integers nums and an integer target, return indices of the two numbers such that 
+they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: True
+Explanation: Because nums[0] + nums[1] == 9, we return True.
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: True
+
+Example 3:
+Input: nums = [1,3], target = 6
+Output: False
+'''
+
+def twoSum(nums, target):
+    # initiate start and end variables
+    left = 0
+    right = len(nums) - 1
+    # loop through the list while left is less than right
+    while left < right:
+        # calculate the sum
+        sum = nums[left] + nums[right]
+        # if the sum is equal to the target then 
+        if sum == target:
+            return True
+        elif sum < target:
+            left += 1
+        else:
+            right -= 1
+    return False
+
+nums = [2,7,11,15]
+target = 9
+print(twoSum(nums, target))
+nums2 = [3, 2, 4]
+target2 = 6
+print(twoSum(nums2, target2))
+nums3 = [1, 3]
+target3 = 6
+print(twoSum(nums3, target3))
