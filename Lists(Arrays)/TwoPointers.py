b91aef55953a31e44554679809d95b2c2edeb2e6
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-12 12:45:28 -0700

    added comments

--- Lists(Arrays)/TwoPointers.py
+++ Lists(Arrays)/TwoPointers.py
@@ -144,9 +144,31 @@ def remove_Duplicates(nums):
     return result
 
 print(remove_Duplicates([2, 3, 3, 3, 6, 9, 9]))
+# Output: 3
 print(remove_Duplicates([2, 2, 2, 11]))
+# Output: 2
 
 
+'''
+Subarray of numbers less than given product
+
+Problem Statement 
+
+(medium)
+
+Given an array with positive numbers and a target number, 
+find all of its contiguous subarrays whose product is less than the target number.
+
+Example 1:
+Input: [2, 5, 3, 10], target=30 
+Output: [2], [5], [2, 5], [3], [5, 3], [10]
+Explanation: There are six contiguous subarrays whose product is less than the target.
+
+Example 2:
+Input: [8, 2, 6, 5], target=50 
+Output: [8], [2], [8, 2], [6], [2, 6], [5], [6, 5] 
+Explanation: There are seven contiguous subarrays whose product is less than the target.
+'''
 
 def find_subarrays(nums, target):
     result = []
