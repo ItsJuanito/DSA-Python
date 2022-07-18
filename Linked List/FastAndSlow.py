250d028556b070b8bd2d0f120a3d9568333a732b
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-18 12:20:34 -0700

    updated code

--- Linked List/FastAndSlow.py
+++ Linked List/FastAndSlow.py
@@ -16,12 +16,20 @@ sixth.next = third
 
 # this function uses the fast and slow approach to see if there is a loop in the linked list
 def findLoop(head):
+    # set slow pointer to the head
     slow = head
+    # set fast pointer to the head
     fast = head
+    # loop through the linked list
     while (slow and fast and fast.next):
+        # slow will move linear
         slow = slow.next
-        fast = fast.next
+        # fast moves twice as fast as slow
+        fast = fast.next.next
+        # if there is a loop then return True
         if slow == fast:
             return True
+    # if there was no loop present, then return false
+    return False
 
 print(findLoop(first))
