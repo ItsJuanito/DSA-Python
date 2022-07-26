ef047782a1488d514eac4c14568154e67c39972e
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-26 15:11:33 -0700

    added problem statement

--- Lists(Arrays)/CyclicSort.py
+++ Lists(Arrays)/CyclicSort.py
@@ -53,6 +53,23 @@ nums3 = [1, 5, 6, 4, 3, 2]
 print(cyclicSort(nums3))
 # Output: [1, 2, 3, 4, 5 ,6]
 
+'''
+Find All Duplicates
+
+(Medium)
+
+Problem Statement: We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. 
+The array has some duplicates, find all the duplicate numbers without using any extra space.
+
+Example 1:
+Input: [3, 4, 4, 5, 5]
+Output: [4, 5]
+
+Example 2:
+Input: [5, 4, 7, 2, 3, 5, 3]
+Output: [3, 5]
+'''
+
 def find_all_duplicates(nums):
     i = 0
     while i < len(nums):
