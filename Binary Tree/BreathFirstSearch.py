6337ae2a4ed0150d9dcd6374b873b9af9fba3b67
ItsJuanito <jzaragoza9jr@gmail.com>
2022-07-27 12:36:08 -0700

    added level breath first search

--- Binary Tree/BreathFirstSearch.py
+++ Binary Tree/BreathFirstSearch.py
@@ -9,6 +9,32 @@ a.insert(4)
 a.insert(6)
 a.insert(12)
 
+b = TreeNode(4)
+b.insert(2)
+b.insert(10)
+b.insert(8)
+b.insert(5)
+
+'''
+ Tree Structure (a):
+          10
+       /      \
+      4       12
+    /   \    /   \
+  None   6 None  None
+
+Tree Structure (b):
+              4
+          /      \
+         2       10
+        / \      / \
+      None None 8   None
+               /  \
+              5   None
+            /   \
+         None   None
+'''
+
 def breathFirstSearch(root):
     if root == None:
         return []
@@ -25,4 +51,26 @@ def breathFirstSearch(root):
     return result
 
 print(breathFirstSearch(a))
+print(breathFirstSearch(b))
 
+
+def levelBFS(root):
+    if root == None:
+        return []
+    result = []
+    queue = Queue()
+    queue.enqueue(root)
+    while queue.size() > 0:
+        level = []
+        for _ in range(queue.size()):
+            current = queue.dequeue()
+            level.append(current.data)
+            if current.left:
+                queue.enqueue(current.left)
+            if current.right:
+                queue.enqueue(current.right)
+        result.append(level)
+    return result
+
+print(levelBFS(a))
+print(levelBFS(b))
