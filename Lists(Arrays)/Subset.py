0ae14e8576b6ee1610116853411c4280e755c085
ItsJuanito <jzaragoza9jr@gmail.com>
2022-08-11 12:32:28 -0700

    added comments

--- Lists(Arrays)/Subset.py
+++ Lists(Arrays)/Subset.py
@@ -1,14 +1,21 @@
 
 def find_subsets(nums):
-    subset = []
-    subset.append([])
-
+    # create a subsets list
+    subsets = []
+    # append an empty set to the list
+    subsets.append([])
+    # loop through each element in nums
     for n in nums:
-        for i in range(len(subset)):
-            temp = subset[i].copy()
+        # loop through each element in subset
+        for i in range(len(subsets)):
+            # copy the ith element and store it in temp list
+            temp = subsets[i].copy()
+            # append the current element to the temp list
             temp.append(n)
-            subset.append(temp)
-    return subset
+            # add the temp list to the subset
+            subsets.append(temp)
+    # return the subset
+    return subsets
 
 nums = [1, 3]
 print(find_subsets(nums))
