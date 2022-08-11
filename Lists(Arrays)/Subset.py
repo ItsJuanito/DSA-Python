acc2c3fad2e4c4b16820ab501739692670fa8e71
ItsJuanito <jzaragoza9jr@gmail.com>
2022-08-11 12:34:53 -0700

    added problem statement

--- Lists(Arrays)/Subset.py
+++ Lists(Arrays)/Subset.py
@@ -1,4 +1,20 @@
 
+'''
+Find All Subsets of a Set
+
+(easy)
+
+Problem Statement: Given a set with distinct elements, find all of its distinct subsets.
+
+Example 1:
+Input: [1, 3]
+Output: [], [1], [3], [1,3]
+
+Example 2:
+Input: [1, 5, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
+'''
+
 def find_subsets(nums):
     # create a subsets list
     subsets = []
