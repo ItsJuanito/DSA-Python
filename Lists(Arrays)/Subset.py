3265f7671e0af46d528ba6c3793722aed7c999b0
ItsJuanito <jzaragoza9jr@gmail.com>
2022-08-16 15:17:12 -0700

    added problem statement

--- Lists(Arrays)/Subset.py
+++ Lists(Arrays)/Subset.py
@@ -39,6 +39,22 @@ nums2 = [1, 5, 3]
 print(find_subsets(nums2))
 # Output: [[], [1], [5], [1, 5], [3], [1, 3], [5, 3], [1, 5, 3]]
 
+'''
+Find Subset with Duplicate Values
+
+(easy)
+
+Problem Statement: Given a set of numbers that might contain duplicates, find all of 
+its distinct subsets.
+
+Example 1:
+Input: [1, 3, 3]
+Output: [], [1], [3], [1,3], [3,3], [1,3,3]
+
+Example 2:
+Input: [1, 5, 3, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3], [3,3], [1,3,3], [3,3,5], [1,5,3,3] 
+'''
 
 def find_subsets_with_duplicate(nums):
     # sort the nums list
