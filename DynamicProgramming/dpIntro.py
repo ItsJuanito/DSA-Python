061c4ef0491adb0bbdd7727bdf946443b8c16889
ItsJuanito <jzaragoza9jr@gmail.com>
2023-03-03 10:34:23 -0800

    added integer replacement function

--- DynamicProgramming/dpIntro.py
+++ DynamicProgramming/dpIntro.py
@@ -1,5 +1,56 @@
 from functools import cache
 
+
+'''
+397. Integer Replacement
+
+(medium)
+
+Given a positive integer n, you can apply one of the following operations:
+
+If n is even, replace n with n / 2.
+If n is odd, replace n with either n + 1 or n - 1.
+Return the minimum number of operations needed for n to become 1.
+
+Example 1:
+Input: n = 8
+Output: 3
+Explanation: 8 -> 4 -> 2 -> 1
+
+Example 2:
+Input: n = 7
+Output: 4
+Explanation: 7 -> 8 -> 4 -> 2 -> 1
+or 7 -> 6 -> 3 -> 2 -> 1
+
+Example 3:
+Input: n = 4
+Output: 2
+'''
+
+def integerReplacement(n):
+    result = []
+    @cache
+    def dp(num, turns=0):
+        if num == 1:
+            result.append(turns)
+            return
+        if num % 2 == 0:
+            dp(num//2, turns + 1)
+        else:
+            dp(num + 1, turns + 1)
+            dp(num - 1, turns + 1)
+    dp(n)
+    return min(result)
+
+print(integerReplacement(8))
+# Output: 3
+print(integerReplacement(7))
+# Output: 4
+print(integerReplacement(4))
+# Output: 2
+
+
 '''
 62. Unique Paths
 
