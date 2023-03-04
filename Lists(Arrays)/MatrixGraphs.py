ed603a882fa96d78badb85754d8bd5ebcbeb7bb1
ItsJuanito <jzaragoza9jr@gmail.com>
2023-03-03 16:35:53 -0800

    added matrix function

--- Lists(Arrays)/MatrixGraphs.py
+++ Lists(Arrays)/MatrixGraphs.py
@@ -154,3 +154,91 @@ print(isToeplitzMatrix(matrix))
 matrix = [[1,2],[2,2]]
 print(isToeplitzMatrix(matrix))
 # Output: True
+
+'''
+2120. Execution of All Suffix Instructions Staying in a Grid
+
+(medium)
+
+There is an n x n grid, with the top-left cell at (0, 0) and the bottom-right cell at (n - 1, n - 1). 
+You are given the integer n and an integer array startPos where startPos = [startrow, startcol] indicates 
+that a robot is initially at cell (startrow, startcol).
+
+You are also given a 0-indexed string s of length m where s[i] is the ith instruction for the robot: 
+'L' (move left), 'R' (move right), 'U' (move up), and 'D' (move down).
+
+The robot can begin executing from any ith instruction in s. It executes the instructions one by one towards 
+the end of s but it stops if either of these conditions is met:
+
+The next instruction will move the robot off the grid.
+There are no more instructions left to execute.
+Return an array answer of length m where answer[i] is the number of 
+instructions the robot can execute if the robot begins executing from the ith instruction in s.
+
+Example 1:
+Input: n = 3, startPos = [0,1], s = "RRDDLU"
+Output: [1,5,4,3,1,0]
+Explanation: Starting from startPos and beginning execution from the ith instruction:
+- 0th: "RRDDLU". Only one instruction "R" can be executed before it moves off the grid.
+- 1st:  "RDDLU". All five instructions can be executed while it stays in the grid and ends at (1, 1).
+- 2nd:   "DDLU". All four instructions can be executed while it stays in the grid and ends at (1, 0).
+- 3rd:    "DLU". All three instructions can be executed while it stays in the grid and ends at (0, 0).
+- 4th:     "LU". Only one instruction "L" can be executed before it moves off the grid.
+- 5th:      "U". If moving up, it would move off the grid.
+
+Example 2:
+Input: n = 2, startPos = [1,1], s = "LURD"
+Output: [4,1,0,0]
+Explanation:
+- 0th: "LURD".
+- 1st:  "URD".
+- 2nd:   "RD".
+- 3rd:    "D".
+
+Example 3:
+Input: n = 1, startPos = [0,0], s = "LRUD"
+Output: [0,0,0,0]
+Explanation: No matter which instruction the robot begins execution from, it would move off the grid.
+'''
+
+moves = {
+    'U' : [-1, 0], 
+    'R' : [0, 1], 
+    'D' : [1, 0], 
+    'L' : [0, -1]
+    }
+def isValid(curr, next, n):
+    x = curr[0] + next[0]
+    y = curr[1] + next[1]
+    return x < n and y < n and x >= 0 and y >= 0
+
+def executeInstructions(n, startPos, s):
+    result = []
+    for i in range(len(s)):
+        count = 0
+        curr_pos = startPos
+        for j in range(i, len(s)):
+            if isValid(curr_pos, moves[s[j]], n):
+                new_pos = moves[s[j]]
+                curr_pos = [(curr_pos[0] + new_pos[0]), (curr_pos[1] + new_pos[1])]
+                count += 1
+            else:
+                break
+        result.append(count)
+    return result
+
+n = 3
+startPos = [0,1]
+s = "RRDDLU"
+print(executeInstructions(n, startPos, s))
+# Output: [1,5,4,3,1,0]
+n = 2
+startPos = [1,1]
+s = "LURD"
+print(executeInstructions(n, startPos, s))
+# Output: [4,1,0,0]
+n = 1
+startPos = [0,0]
+s = "LRUD"
+print(executeInstructions(n, startPos, s))
+# Output: [0,0,0,0]
