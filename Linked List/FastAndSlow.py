4595cb99199e234e52b3a597081241b5408c8bb2
ItsJuanito <jzaragoza9jr@gmail.com>
2023-03-10 13:22:54 -0800

    added new functions

--- Linked List/FastAndSlow.py
+++ Linked List/FastAndSlow.py
@@ -61,3 +61,43 @@ print(findLoop(first))
 # Output: True
 print(findLoop(this))
 # Ouput: False
+
+'''
+142. Linked List Cycle II
+
+(medium)
+
+Given the head of a linked list, return the node where the cycle begins. If there is no 
+cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached 
+again by continuously following the next pointer. Internally, pos is used to denote the 
+index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there 
+is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+
+Example 1:
+Input: head = [a, b, c, d, e], pos = 2
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+'''
+
+def detectCycle(head):
+    if not head:
+        return None
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+            break
+    else:
+        return None
+    while head != slow:
+        head = head.next
+        slow = slow.next
+    return head
+
+print(detectCycle(first).data)
