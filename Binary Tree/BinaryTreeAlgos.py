b5c4dd4c691e4ea9b28db43fd1a999232d0c9252
ItsJuanito <jzaragoza9jr@gmail.com>
2023-03-14 12:46:22 -0700

    added new problem

--- Binary Tree/BinaryTreeAlgos.py
+++ Binary Tree/BinaryTreeAlgos.py
@@ -237,3 +237,46 @@ print(maxLevelSum(a))
 # Output: 2
 print(maxLevelSum(c))
 # Output: 3
+
+'''
+129. Sum Root to Leaf Numbers
+
+(medium)
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so 
+that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+Example 1:
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+'''
+
+def sumNumbers(root):
+    nums = []
+    def dfs(root, string=""):
+        if not root:
+            return
+        string += str(root.data)
+        if (root.left is None) and (root.right is None):
+            nums.append(string)
+            string=""
+            return
+        dfs(root.left, string)
+        dfs(root.right, string)
+    dfs(root)
+    nums = [int(num) for num in nums]
+    return sum(nums)
+
+print(sumNumbers(a))
+# Output: 1107
